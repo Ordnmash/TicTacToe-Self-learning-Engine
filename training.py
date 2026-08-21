2a70d8a22d5de53f938e2877371b95f4f41e121b
Ordnmash <ordnfxist@gmail.com>
2026-08-21 23:14:39 +0200

    Fix indentation and remove unnecessary line

--- training.py
+++ training.py
@@ -23,7 +23,6 @@ if train:
     # optimizing...
     for p in x.parameters:
       p.data -= (lr * p.grad)
-
   # with who passed as 'o' we only trained specific side of the model which is 'O'
   if who == 'o':
     for p in o.parameters:
